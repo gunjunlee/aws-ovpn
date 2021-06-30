4e7c7f5dfd89c4ff9371722cc4981f9fd68f7552
gunjunlee <gunjunlee97@gmail.com>
2021-07-01 00:51:58 +0900

    wireguard mac

--- vpn.py
+++ vpn.py
@@ -1,7 +1,4 @@
 import subprocess
-from sys import stderr, stdin, stdout
-import tempfile
-import json
 import time
 import os
 from pathlib import Path
@@ -9,12 +6,14 @@ import platform
 import csv
 import json
 import sys
+import signal
+import configparser
+import argparse
 import boto3
+import botocore
 from botocore.config import Config
 import pdb
 
-SecurityGroupIds = [""]  #T
-path_aws_key = ""  #T
 SSH_CMD = "ssh -o StrictHostKeyChecking=no -i {path_aws_key} ubuntu@{aws_ip}"
 SCP_CMD = "scp -o StrictHostKeyChecking=no -i {path_aws_key} ubuntu@{aws_ip}"
 SSH_WAIT_CMD = SSH_CMD + " ls"
@@ -23,6 +22,8 @@ SET_WG_SERVER_CMD =  SSH_CMD + \
                      " && sudo bash wireguard-install.sh" + \
                      " && sudo cp /root/client.conf ~/\""
 GET_WG_CONF_CMD = SCP_CMD + ":~/client.conf {conf_path}"
+WG_UP = "sudo wg-quick up {conf_path}"
+WG_DOWN = "sudo wg-quick down {conf_path}"
 AWS_REGIONS = {
     "tokyo": "ap-northeast-1",
     "seoul": "ap-northeast-2",
@@ -38,7 +39,6 @@ AWS_REGIONS = {
 }
 
 cur_dir = Path(__file__).parent
-path_config_template = cur_dir / "config_template.conf"
 path_credential = cur_dir / "credentials.csv"
 path_cache = cur_dir / "cache.json"
 dir_conf = cur_dir / "configs"
@@ -57,18 +57,27 @@ def connect_linux():
     pass
 
 
-def get_region(default_region):
+def get_region(region):
+    if region is not None:
+        if region in AWS_REGIONS.keys():
+            return AWS_REGIONS[region]
+        elif region in AWS_REGIONS.values():
+            return region
+        print("There is no region {region}".format(region=region))
+
+    default_region = "tokyo"
     while True:
         region = input(
             "Enter the VPN server region {regions} (default: {default_region}): "
-            .format(regions=list(AWS_REGIONS.keys()), default_region=default_region)).strip()
+            .format(regions=list(AWS_REGIONS.keys()),
+                    default_region=default_region)).strip()
         if len(region) == 0:
             region = default_region
         if region in AWS_REGIONS.keys():
             return AWS_REGIONS[region]
         elif region in AWS_REGIONS.values():
             return region
-        print("There is no region named {region}.".format(region=region))
+        print("There is no region {region}.".format(region=region))
 
 
 def check_ssh_key(path_aws_key):
@@ -76,6 +85,7 @@ def check_ssh_key(path_aws_key):
         raise RuntimeError(
             "Permission of the ssh identity file is not owner read only.\n" \
             "Try: chmod 400 {path_aws_key}".format(path_aws_key=path_aws_key))
+
 
 def aws_wait_ssh(path_aws_key, aws_ip):
     cmd = SSH_WAIT_CMD.format(path_aws_key=path_aws_key, aws_ip=aws_ip)
@@ -87,6 +97,7 @@ def aws_wait_ssh(path_aws_key, aws_ip):
             "Cannot connect Instance(id: {instance_id}, ip: {aws_ip}) yet. wait for 5 seconds.."
             .format(instance_id=instance_id, aws_ip=aws_ip))
         time.sleep(5)
+
 
 def set_wg_server(path_aws_key, aws_ip):
     cmd = SET_WG_SERVER_CMD.format(path_aws_key=path_aws_key, aws_ip=aws_ip)
@@ -102,30 +113,30 @@ def set_wg_server(path_aws_key, aws_ip):
 
 
 def get_wg_conf(path_aws_key, aws_ip, conf_path):
-    proc = subprocess.run(GET_WG_CONF_CMD.format(path_aws_key=path_aws_key, aws_ip=aws_ip, conf_path=conf_path), shell=True)
+    proc = subprocess.run(GET_WG_CONF_CMD.format(path_aws_key=path_aws_key,
+                                                 aws_ip=aws_ip,
+                                                 conf_path=conf_path),
+                          shell=True)
     if proc.returncode != 0:
-        raise RuntimeError("Error occured during downloading wireguard config from server")
+        raise RuntimeError(
+            "Error occured during downloading wireguard config from server")
 
 
-def aws_run_instance(ec2):
+def aws_run_instance(ec2, sg_id):
     ret = ec2.run_instances(
         MaxCount=1,
         MinCount=1,
         ImageId="ami-0df99b3a8349462c6",  # ubuntu 20.04 base
         KeyName="aws-tokyo",
         InstanceType="t2.micro",
-        SecurityGroupIds=SecurityGroupIds,
-        TagSpecifications=[
-            {
-                "ResourceType": "instance",
-                "Tags": [
-                    {
-                        "Key": "Name",
-                        "Value": "wg-server",
-                    }
-                ]
-            }
-        ],
+        SecurityGroupIds=sg_id,
+        TagSpecifications=[{
+            "ResourceType": "instance",
+            "Tags": [{
+                "Key": "Name",
+                "Value": "wg-server",
+            }]
+        }],
     )
 
     return ret["Instances"][0]["InstanceId"]
@@ -138,35 +149,49 @@ def aws_wait_running(ec2, instance_id):
         if state == "running":
             break
         print(
-            "Instance{instance_id} is not running (now: {state}). wait for 5 seconds.."
+            "Instance (id={instance_id}) is not running (now: {state}). wait for 5 seconds.."
             .format(instance_id=instance_id, state=state))
         time.sleep(5)  # wait for 5 secs
+
 
 def aws_get_ip(ec2, instance_id):
     ret = ec2.describe_instances(InstanceIds=[instance_id])
     ip = ret["Reservations"][0]["Instances"][0]["PublicIpAddress"]
     return ip
 
-def aws_start_instance():
-    pass
+
+def aws_is_instance_usable(ec2, instance_id):
+    try:
+        ret = ec2.describe_instances(InstanceIds=[instance_id])
+        state = ret["Reservations"][0]["Instances"][0]["State"]["Name"].lower()
+        if state == "shutting-down" or state == "terminated":
+            print("instance (id={instance_id}) is not usable (state={state})".
+                  format(instance_id=instance_id, state=state))
+            return False
+    except botocore.exceptions.ClientError as e:
+        if e.response["Error"]["Code"] == "InvalidInstanceID.NotFound":
+            print("Cannot find instance (id = {instance_id})".format(
+                instance_id=instance_id))
+            return False
+    return True
 
 
-def aws_terminate_instance():
-    pass
+def aws_start_instance(ec2, instance_id):
+    ret = ec2.start_instances(InstanceIds=[instance_id])
 
 
-def aws_check_instance():
-    pass
-    stats["Reservations"][0]["Instances"][0]["Tags"]
-    import pdb; pdb.set_trace()
+def aws_stop_instance(ec2, instance_id):
+    ret = ec2.stop_instances(InstanceIds=[instance_id])
 
 
-def aws_check_security_group():
-    pass
+def aws_get_security_group_id():
+    try:
+        ret = ec2.describe_security_groups(GroupNames=["AWS-WIREGUARD-VPN"])
+    except botocore.exceptions.ClientError as e:
+        if e.response["Error"]["Code"] == "InvalidGroup.NotFound":
+            return None
+    return ret["SecurityGroups"][0]["GroupId"]
 
-
-def aws_make_security_group():
-    pass
 
 def get_cred_key():
     try:
@@ -190,7 +215,49 @@ def get_cred_key():
             path_credential=path_credential.absolute()))
         exit(1)
 
+
+def wg_up(conf_path):
+    cmd = WG_UP.format(conf_path=conf_path)
+    ret = subprocess.run(cmd, shell=True)
+
+
+def wg_down(conf_path):
+    cmd = WG_DOWN.format(conf_path=conf_path)
+    ret = subprocess.run(cmd, shell=True)
+
+
+def signal_handler(sig, frame):
+    wg_down(conf_path=conf_path)
+    aws_stop_instance(ec2, instance_id)
+
+    cache[aws_region] = {
+        "instance-id": instance_id,
+        "conf-path": conf_path,
+        "path-aws-key": path_aws_key,
+    }
+    with path_cache.open("w") as cache_file:
+        json.dump(cache, cache_file)
+
+
+def update_config(conf_path, ip):
+    config = configparser.ConfigParser()
+    config.read(conf_path)
+    _, port = config["Peer"]["Endpoint"].split(":")
+    config["Peer"]["Endpoint"] = "{ip}:{port}".format(ip=ip, port=port)
+    with open(conf_path, "w") as f:
+        config.write(f)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--aws-key", type=str, help="path to aws key")
+    parser.add_argument("--region", type=str, help="region")
+    args = parser.parse_args()
+    return args
+
+
 if __name__ == "__main__":
+    args = parse_args()
 
     access_key, secret_key = get_cred_key()
 
@@ -202,23 +269,54 @@ if __name__ == "__main__":
     except FileNotFoundError:
         pass
 
-    aws_region = get_region(cache.get("aws_region", "tokyo"))
-    conf_path = (dir_conf / aws_region).absolute() + ".conf"
-
-    config = Config(
-        region_name = aws_region,
-    )
-
+    aws_region = get_region(cache.get("aws_region", args.region))
+    config = Config(region_name=aws_region, )
     ec2 = boto3.client("ec2",
                        config=config,
                        aws_access_key_id=access_key,
                        aws_secret_access_key=secret_key)
 
-    instance_id = aws_run_instance(ec2)
-    aws_wait_running(ec2, instance_id)
-    ip = aws_get_ip(ec2, instance_id)
+    sg_id = aws_get_security_group_id()
+    if sg_id is None:
+        print(
+            "Cannot find security group for AWS WIREGUARD VPN (region: {region})"
+            .format(region=aws_region))
+        print("Set security group according to the guide in README")
+        exit(1)
 
-    check_ssh_key(path_aws_key)
-    aws_wait_ssh(path_aws_key=path_aws_key, aws_ip=ip)
-    set_wg_server(path_aws_key=path_aws_key, aws_ip=ip)
-    get_wg_conf(path_aws_key=path_aws_key, aws_ip=ip, conf_path=conf_path)
+    is_ready = False
+    if aws_region in cache.keys():
+        c = cache[aws_region]
+        instance_id = c["instance-id"]
+        conf_path = c["conf-path"]
+        path_aws_key = c["path-aws-key"]
+        check_ssh_key(path_aws_key)
+
+        if aws_is_instance_usable(ec2, instance_id):
+            aws_start_instance(ec2, instance_id)
+            aws_wait_running(ec2, instance_id)
+            ip = aws_get_ip(ec2, instance_id)
+            aws_wait_ssh(path_aws_key=path_aws_key, aws_ip=ip)
+            update_config(conf_path, ip)
+            is_ready = True
+    if not is_ready:
+        conf_path = str((dir_conf / aws_region).absolute()) + ".conf"
+        instance_id = aws_run_instance(ec2, sg_id)
+        aws_wait_running(ec2, instance_id)
+        ip = aws_get_ip(ec2, instance_id)
+        if args.aws_key is not None:
+            path_aws_key = args.aws_key
+        else:
+            path_aws_key = input(
+                "Enter the path to aws key (region: {region}): ".format(
+                    region=aws_region))
+        check_ssh_key(path_aws_key)
+
+        aws_wait_ssh(path_aws_key=path_aws_key, aws_ip=ip)
+        set_wg_server(path_aws_key=path_aws_key, aws_ip=ip)
+        get_wg_conf(path_aws_key=path_aws_key, aws_ip=ip, conf_path=conf_path)
+
+    wg_up(conf_path=conf_path)
+
+    signal.signal(signal.SIGINT, signal_handler)
+    signal.pause()
